671025869a55ffa6c6d436fa32c125747bec18b8
anugrahkresnaya <anugrahkresnaya@gmail.com>
2025-02-25 17:28:20 +0700

    feat: add navbar component

---
+++ src/app/components/navbar.tsx
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return (
+    <nav className="flex justify-between">
+      <h1>Watchlist</h1>
+      <ul className="flex justify-evenly">
+        <li>Create</li>
+        <li>Login</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
