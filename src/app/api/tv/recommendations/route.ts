9a34a829a68c611fe9154ba1300b89537e331dd3
= <=>
2025-05-07 06:49:32 +0700

    feat: add recommendation

---
+++ src/app/api/tv/recommendations/route.ts
@@ -0,0 +1,34 @@
+import axios from "axios";
+import { NextResponse } from "next/server"
+
+const API_KEY_TMDB = process.env.NEXT_PUBLIC_API_KEY_TMDB;
+const BASE_URL = 'https://api.themoviedb.org/3';
+
+export async function GET(req: Request) {
+  const { searchParams } = new URL(req.url)
+  const tvId = searchParams.get('id')
+
+  if (!tvId) {
+    return NextResponse.json(
+      { error: 'Tv ID is required' },
+      { status: 400 }
+    );
+  }
+
+  try {
+    const { data } = await axios.get(`${BASE_URL}/tv/${tvId}/recommendations`, {
+      headers: {
+        Accept: 'application/json',
+        Authorization: `Bearer ${API_KEY_TMDB}`
+      }
+    })
+
+    return NextResponse.json(data, { status: 200 });
+  } catch (error) {
+    console.error('Error while fetching tv recommendations', error);
+    return NextResponse.json(
+      { error: 'Failed to fetch data' },
+      { status: 500 }
+    );
+  }
+}
